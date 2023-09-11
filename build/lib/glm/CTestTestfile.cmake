acd417264839fe9d75e5630bfdc3f7e44ace4581
JoeSteeb <josteeb@gmail.com>
2023-09-10 21:55:46 -0700

    removed build

--- build/lib/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/joest/Desktop/code/Vulkan3DEngine/lib/glm
-# Build directory: C:/Users/joest/Desktop/code/Vulkan3DEngine/build/lib/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
